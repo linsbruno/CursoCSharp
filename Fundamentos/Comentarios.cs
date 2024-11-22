cb9bd1a722224c0bbf5b9f10be6b9bd959babc71
Bruno Lins <linslimabruno@gmail.com>
2024-11-22 18:43:56 -0300

    adicionando comentarios em fundamentos

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+
+    /// <summary>
+    /// 
+    /// </summary>
+     class Comentarios {
+        public static void Executar() {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor");
+            Console.WriteLine(" o C# tem o xml comments , basta colocar ///");
+            Console.WriteLine("Comentários de multiplas linhas é com /* */");
+            /*
+             * 
+             */
+        }
+    }
+}
+
